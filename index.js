902cef8e6f0e863810aaea3348f23b93ef9d8975
TrungNNg <tnguy384@mail.ccsf.edu>
2020-09-28 16:05:40 -0400

    add logic to toggle theme button

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Toggle nav
 const get = id => document.getElementById(id);
 
 const open = get('menu-btn');
@@ -11,4 +12,53 @@ open.addEventListener('click', function(){
 
 exit.addEventListener('click', function(){
     nav.classList.remove('nav-open');
+})
+
+/* 
+--body-bg: #303A52;
+--panel-bg: #39445F;
+--nav-bg: #2C354A;
+--text-primary: #ffffff;
+--text-secondary: #A5B2D3;
+--text-bg: #FC85AE;
+--svg-fill: #ffffff;
+
+//light
+--body-bg: #ffffff;
+--panel-bg: #6baec5;
+--nav-bg: #ffffff;
+--text-primary: #000000;
+--text-secondary: #ffffff;
+--text-bg: #07436e;
+--svg-fill: #000000;
+*/
+
+//Change theme
+const toggle = document.getElementById('toggle-theme');
+const root = document.documentElement;
+
+let isDarkMode = true;
+
+toggle.addEventListener('click', () => {
+    if(isDarkMode){
+        toggle.innerText = 'Dark Theme';
+        root.style.setProperty('--body-bg', '#ffffff');
+        root.style.setProperty('--panel-bg', '#6baec5');
+        root.style.setProperty('--nav-bg', '#f5f5f5');
+        root.style.setProperty('--text-primary', '#000000');
+        root.style.setProperty('--text-secondary', '#ffffff');
+        root.style.setProperty('--text-bg', '#07436e');
+        root.style.setProperty('--svg-fill', '#000000');
+        isDarkMode = false;
+    }else{
+        toggle.innerText = 'Light Theme';
+        root.style.setProperty('--body-bg', '#303A52');
+        root.style.setProperty('--panel-bg', '#39445F');
+        root.style.setProperty('--nav-bg', '#2C354A');
+        root.style.setProperty('--text-primary', '#ffffff');
+        root.style.setProperty('--text-secondary', '#A5B2D3');
+        root.style.setProperty('--text-bg', '#FC85AE');
+        root.style.setProperty('--svg-fill', '#ffffff');
+        isDarkMode = true;
+    }
 })
