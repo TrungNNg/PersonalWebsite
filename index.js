b077a66443972287353e775d545e5133db3745c7
TrungNNg <tnguy384@mail.ccsf.edu>
2020-09-27 23:17:54 -0400

    add JS logic to svg in nav and header to toggle nav

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('hello')
+const get = id => document.getElementById(id);
+
+const open = get('menu-btn');
+const exit = get('exit-btn');
+const nav = get('nav')
+
+open.addEventListener('click', function(){
+    console.log('open button')
+    nav.classList.add('nav-open')
+})
+
+exit.addEventListener('click', function(){
+    nav.classList.remove('nav-open');
+})
